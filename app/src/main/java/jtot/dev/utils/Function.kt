683889a37ab61ef59a111943e83d5510f83476d6
jkey20 <helios2sic@gmail.com>
2024-01-11 14:54:10 +0900

    POL-47 MainFragment 변경
    - Play 버튼 선택 시 PlayActivity 이동처리 및 데이터 전달
    - MainFragment.xml 리팩토링
    - Material Calendarview xml preivew 오류를 해결하기 위해 버전 다운그레이드
    - Folder Dummy 데이터를 실제와 비슷하게 변경
    - Dummy 데이터에 날짜값을 넣기 위해서 날짜 함수 추가

--- app/src/main/java/jtot/dev/utils/Function.kt
+++ app/src/main/java/jtot/dev/utils/Function.kt
@@ -2,6 +2,8 @@ package jtot.dev.utils
 
 import jtot.dev.model.Schedule
 import jtot.dev.model.Todo
+import java.util.Calendar
+import java.util.Locale
 
 fun getTodoList(schedule: Schedule): MutableList<Todo> {
     return mutableListOf<Todo>().apply {
@@ -11,4 +13,9 @@ fun getTodoList(schedule: Schedule): MutableList<Todo> {
             }
         }
     }
+}
+
+fun getToday(): String {
+    val cal = Calendar.getInstance(Locale.KOREA)
+    return cal.time.convertToFormat()
 }
