978fe16ae85c7450b4c20d51e23badcccd86aaad
Edwin247 <cheewr85@daum.net>
2024-01-11 12:53:59 +0900

    POL-31 테스트 코드 작성

---
+++ app/src/test/java/jtot/dev/MainViewModelUnitTest.kt
@@ -0,0 +1,34 @@
+package jtot.dev
+
+class MainViewModelUnitTest {
+//    @Test
+//    fun testWriteFolder() {
+//        // Given
+//        val mockFolder = mock(Folder::class.java)
+//        val mockDatabaseReference = mock(DatabaseReference::class.java)
+//        val viewModel =
+//            MainViewModel().apply {
+//                myRef = mockDatabaseReference
+//            }
+//
+//        // When
+//        viewModel.writeFolder(mockFolder)
+//
+//        // Then
+//        verify(mockDatabaseReference).setValue(mockFolder)
+//    }
+//
+//    @Test
+//    fun testReadFolder() {
+//        val mockDataSnapshot = mock(DataSnapshot::class.java)
+//        val mockFolder = mock(Folder::class.java)
+//        `when`(mockDataSnapshot.getValue(Folder::class.java)).thenReturn(mockFolder)
+//
+//        val viewModel = MainViewModel()
+//        viewModel.myRef = mock(DatabaseReference::class.java)
+//
+//        viewModel.readFolder()
+//
+//        verify(viewModel.myRef).addValueEventListener(any())
+//    }
+}
