42f8574fa0dfc797462812341a5f557285303be8
jkey20 <helios2sic@gmail.com>
2024-01-14 17:13:01 +0900

    POL-57 Side sheet 디자인 구현
    ##### Modal Side Sheet width를 조절하는 방법이 1.12.0-alpha03 버전부터 지원됨에 따라 Material Library 버전 업그레이드
    - [관련 이슈](https://github.com/material-components/material-components-android/issues/3492)
    - [문제가 해결된 커밋](https://github.com/material-components/material-components-android/commit/3b278d709756e0a0d2ba856697ef4bcb76ae1544)
    
    etc
    - ImageButton 컴포넌트로 사용하던 아이콘 버튼을 Button 컴포넌트에 style을 입혀 Material 버튼 방식으로 변경

--- app/src/main/java/jtot/dev/feature/main/MainActivity.kt
+++ app/src/main/java/jtot/dev/feature/main/MainActivity.kt
@@ -2,14 +2,36 @@ package jtot.dev.feature.main
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import com.google.android.material.sidesheet.SideSheetBehavior
 import jtot.dev.R
 import jtot.dev.base.BaseActivity
 import jtot.dev.databinding.ActivityMainBinding
 import jtot.dev.feature.play.PlayActivity
 
 class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
+    private val standardSideSheetBehavior: SideSheetBehavior<View> by lazy {
+        SideSheetBehavior.from(binding.sideSheet)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        binding.babAppbar.setOnMenuItemClickListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.item_main_today -> {
+                    standardSideSheetBehavior.expand()
+                    true
+                }
+
+                else -> false
+            }
+        }
+
+        binding.btnClose.setOnClickListener {
+            standardSideSheetBehavior.hide()
+        }
+
         binding.fabPlay.setOnClickListener {
             Intent(this, PlayActivity::class.java).apply {
             }.run(::startActivity)
